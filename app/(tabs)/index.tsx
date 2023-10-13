e630fa60c6fd00e9566b19a5f338101df6ba6170
fiVe [Glen Andrew] <gcbulaong@up.edu.ph>
2023-10-13 12:11:21 +0800

    Setup NativeWind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,39 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
+// DATA
+import jsonData from '../../utils/categories.json';
+
+// COMPONENTS
+import ImageButton from '../../components/buttons/ImageButton';
+
 export default function TabOneScreen() {
+  const baseLocation = '../../assets/images/';
+  // const fileName = jsonData[1].imageSRC;
+  const fileName = 'beverages.png';
+  // const sample = {
+  //   '0' : 'appetizers.png',
+  //   '1' : 'beverages.png'
+  // }
+  const imageLocation = baseLocation + fileName;
+  const image = require(imageLocation);
+
+  const pressState = () => {
+    console.log('Button Pressed')
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      {/* <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
+
+      <ImageButton
+        image={image}
+        text={jsonData[1].name}
+        onPress={pressState}
+      />
+
     </View>
   );
 }
