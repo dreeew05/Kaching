a6c84ed35211967fa783a1865d6817176418170a
fiVe [Glen Andrew] <gcbulaong@up.edu.ph>
2023-10-13 12:14:24 +0800

    Revert "Create Button Interface"
    This reverts commit 169dde373414d49948c4f0b78be30179b2dd5bd3.

--- components/buttons/interfaces/BaseButtonInterface.tsx
+++
@@ -1,7 +0,0 @@
-import { StyleProp, ViewStyle } from "react-native";
-
-export interface BaseButtonInterface {
-    text? : string,
-    baseStyle? : StyleProp<ViewStyle>
-    onPress : () => void
-}
--- components/buttons/interfaces/ImageButtonInterface.tsx
+++
@@ -1,7 +0,0 @@
-import { ImageSourcePropType, ImageStyle, StyleProp } from "react-native";
-import { BaseButtonInterface } from "./BaseButtonInterface";
-
-export interface ImageButtonInterface extends BaseButtonInterface {
-    image : ImageSourcePropType
-    imageStyle? : StyleProp<ImageStyle>
-}
