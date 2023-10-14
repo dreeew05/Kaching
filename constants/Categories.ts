39b30c9a5439224db4acfaf2579baf01e7eab9e7
Glen Andrew <gcbulaong@up.edu.ph>
2023-10-14 10:13:07 +0800

    Added Additional Categories

--- constants/Categories.ts
+++ constants/Categories.ts
@@ -15,7 +15,32 @@ const categories : CategoryProps[] = [
         id : 10002,
         name : 'Dessert',
         image : require('../assets/images/dessert.jpg')
-    }
+    },
+    {
+        id : 10003,
+        name : 'Main Course',
+        image : require('../assets/images/main_course.jpg')
+    },
+    {
+        id : 10004,
+        name : 'Pizza',
+        image : require('../assets/images/pizza.jpg')
+    },
+    {
+        id : 10005,
+        name : 'Salad',
+        image : require('../assets/images/salad.jpg')
+    },
+    {
+        id : 10006,
+        name : 'Side Dish',
+        image : require('../assets/images/side_dish.jpg')
+    },
+    {
+        id : 10007,
+        name : 'Special',
+        image : require('../assets/images/special.jpg')
+    },
 ]
 
 export default categories;
