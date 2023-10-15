e65e70a036f7e870bb0ffe3ec0963d20654eb026
Glen Andrew <gcbulaong@up.edu.ph>
2023-10-15 14:15:18 +0800

    Configured for Backend

--- constants/Categories.ts
+++ constants/Categories.ts
@@ -1,46 +1,18 @@
-import { CategoryProps } from "../components/interfaces/CategoryProps";
+import { ImageSourcePropType } from "react-native";
 
-const categories : CategoryProps[] = [
-    {
-        id : 10000,
-        name : 'Appetizer',
-        image : require('../assets/images/appetizer.jpg')
-    },
-    {
-        id : 10001,
-        name : 'Beverages',
-        image : require('../assets/images/beverages.jpg')
-    },
-    {
-        id : 10002,
-        name : 'Dessert',
-        image : require('../assets/images/dessert.jpg')
-    },
-    {
-        id : 10003,
-        name : 'Main Course',
-        image : require('../assets/images/main_course.jpg')
-    },
-    {
-        id : 10004,
-        name : 'Pizza',
-        image : require('../assets/images/pizza.jpg')
-    },
-    {
-        id : 10005,
-        name : 'Salad',
-        image : require('../assets/images/salad.jpg')
-    },
-    {
-        id : 10006,
-        name : 'Side Dish',
-        image : require('../assets/images/side_dish.jpg')
-    },
-    {
-        id : 10007,
-        name : 'Special',
-        image : require('../assets/images/special.jpg')
-    },
-]
+interface ConstantCategoryProps {
+    [id : number] : ImageSourcePropType
+}
+
+const categories : ConstantCategoryProps = {
+    10000 : require('../assets/images/appetizer.jpg'),
+    10001 : require('../assets/images/beverages.jpg'),
+    10002 : require('../assets/images/dessert.jpg'),
+    10003 : require('../assets/images/main_course.jpg'),
+    10004 : require('../assets/images/pizza.jpg'),
+    10005 : require('../assets/images/salad.jpg'),
+    10006 : require('../assets/images/side_dish.jpg'),
+    10007 : require('../assets/images/special.jpg')
+}
 
 export default categories;
