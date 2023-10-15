da06dc1041c90229a6b54fa6788aaebfcd7f75e9
Glen Andrew <gcbulaong@up.edu.ph>
2023-10-16 02:07:48 +0800

    Update testBeveragesData.ts

--- utils/testBeveragesData.ts
+++ utils/testBeveragesData.ts
@@ -4,16 +4,19 @@ const beverages : TestProductInterface[] = [
     {
         id : 10003,
         name : 'Beer',
+        price : 1.99,
         image: require('../assets/icons/blank.jpg')
     },
     {
         id : 10004,
         name : 'Coca-Cola',
+        price : 1.99,
         image: require('../assets/icons/blank.jpg')
     },
     {
         id : 10005,
         name : 'Pepsi',
+        price : 1.99,
         image: require('../assets/icons/blank.jpg')
     }
 ]
