4a2bd1b67b5a116ffd8aab568b501a809fe2d50f
kryshyr <pbsaluria@up.edu.ph>
2023-10-17 23:50:53 +0800

    Added Menu and Other Pages

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,31 +1,71 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { Alert, Pressable, Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+export default function MenuSettings() {
+  
+  const showAlert = () => {
+    Alert.alert('Show Alert Action', 'This is a dummy action.');
+  };
 
-export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+    <View className=" py-10 marker:flex-1 self-stretch bg-white dark:bg-black">
+    
+      <View className=" flex-row justify-around w-3/4 self-center">
+        <Text className="text-white font-bold  bg-green px-2 rounded-lg ">7</Text>
+        <Text className="text-green font-bold px-2 rounded-full">October</Text>
+        <Text className="text-green font-bold px-2 rounded-full flex items">Wednesday</Text>
+      </View>
+
+      <View className="flex items-center ">
+      <Text className=" px-16 py-3 self-center bg-green text-center rounded-md  text-white text-4xl font-bold m-3">$123.56</Text>
+      <Text className="text-green text-2xl font-bold self-center mb-10 px-2 rounded-full flex items">Total Sales</Text>
+      </View>
+
+        <Link href="/(tabs)/currentEOD" asChild>
+          <Pressable className="bg-transparent w-5/6 self-center py-2 px-4 mt-2 mb-5 ml-2 flex-row justify-between items-center">
+              <Text className="text-green text-xl font-bold">View Current EOD</Text>
+              <FontAwesome5 name="angle-right" size={24} color="black" style={{opacity: 0.5 }} />
+          </Pressable>
+        </Link>
+
+        <Link href="/(tabs)/previousEOD" asChild>
+          <Pressable className="bg-transparent w-5/6 self-center py-2 px-4 mt-2 mb-5 ml-2 flex-row justify-between items-center">
+              <Text className="text-green text-xl font-bold">View Previous EOD</Text>
+              <FontAwesome5 name="angle-right" size={24} color="black" style={{opacity: 0.5 }} />
+          </Pressable>
+        </Link>
+
+        <Link href="/(tabs)/termsOfService" asChild>
+          <Pressable className="bg-transparent w-5/6 self-center py-2 px-4 mt-2 mb-5 ml-2 flex-row justify-between items-center">
+              <Text className="text-green text-xl font-bold">Terms of Service</Text>
+              <FontAwesome5 name="angle-right" size={24} color="black" style={{opacity: 0.5 }} />
+          </Pressable>
+        </Link>
+
+        <Link href="/(tabs)/privacyPolicy" asChild>
+          <Pressable className="bg-transparent w-5/6 self-center py-2 px-4 mt-2 mb-5 ml-2 flex-row justify-between items-center">
+              <Text className="text-green text-xl font-bold">Privacy Policy</Text>
+              <FontAwesome5 name="angle-right" size={24} color="black" style={{opacity: 0.5 }} />
+          </Pressable>
+        </Link>
+
+        <Link href="/(tabs)/faqs" asChild>
+          <Pressable className="bg-transparent w-5/6 self-center py-2 px-4 mt-2 mb-5 ml-2 flex-row justify-between items-center">
+              <Text className="text-green text-xl font-bold">FAQs</Text>
+              <FontAwesome5 name="angle-right" size={24} color="black" style={{opacity: 0.5 }} />
+          </Pressable>
+        </Link>
+
+        <Pressable className="bg-transparent w-4/6 self-center mt-10 bg-green items-center rounded-full py-2 px-4 mb-5 ml-2" 
+          onPress={showAlert}>
+
+          <Text className="text-white text-xl font-bold">End Day</Text>
+        </Pressable>
+
     </View>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
