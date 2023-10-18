ca7f3249ff6ead407222ae25fe3abdad933b19a6
Glen Andrew <gcbulaong@up.edu.ph>
2023-10-18 12:16:00 +0800

    Pahuway Banner

---
+++ app/(tabs)/pahuwayBanner.tsx
@@ -0,0 +1,55 @@
+import { useRouter } from "expo-router";
+import { View, Text, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const router = useRouter();
+
+const gotToHome = () => {
+    // router.push('/(tabs)/w')
+}
+
+const gotToEOD = () => {
+    router.push('/(tabs)/currentEOD')
+}
+
+export default function PahuwayBanner() {
+    return(
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-lg font-bold">
+                Pahuway Na!
+            </Text>
+            <Image
+                className="mt-5 mb-5"
+                source={require('../../assets/icons/eod.png')}
+                style={{width: 300, height: 300}}
+            />
+            <Text>
+                Day Ended Successfully!
+            </Text>
+
+            <View className="mt-7">
+                <View className="h-10 w-60 border-2 border-green
+                    rounded-full flex-row justify-center items-center"
+                >
+                    <TouchableOpacity onPress={gotToEOD}>
+                        <Text className="text-center">
+                            View EOD Report
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <Text className="text-center mt-2 mb-2">Or</Text>
+                <View className="bg-green h-10 w-60 rounded-full
+                    flex-row justify-center items-center"
+                >
+                    <TouchableOpacity>
+                        <Text className="text-center
+                            text-white font-bold"
+                        >
+                            Back to Home
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
