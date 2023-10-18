9f13b192b0982a77d33b1b1020123bf7b1daf77e
kryshyr <pbsaluria@up.edu.ph>
2023-10-18 13:19:06 +0800

    Added Dashboard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -41,6 +41,17 @@ export default function TabOneScreen() {
         </View>
       </Pressable>
 
+      <View className="flex-row mb-5 py-3 px-10 items-center shadow-lg shadow-neutral-600 rounded-lg self-center bg-white">
+        <View>
+          <Text className=" px-7 self-center text-center text-green text-3xl font-bold">$123.56</Text>
+          <Text className=" px-7 self-center text-center text-black text-sm opacity-50 font-base">Total Sales</Text>
+        </View>
+        <Text className=" self-center text-center text-gray text-5xl font-thin">|</Text>
+        <View>
+          <Text className=" px-7 self-center text-center text-green text-3xl font-bold">123</Text>
+          <Text className=" px-7 self-center text-center text-black text-sm opacity-50 font-base">Orders</Text>
+        </View>
+      </View>
       <View className="flex flex-row items-end">
         <Text className="text-3xl font-base ml-5 text-darkgreen">Categories</Text>
         <Pressable className='self-center ml-3'
