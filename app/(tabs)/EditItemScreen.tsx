c5e06562aa03a58cf1fb771af5e338d72061b4af
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2023-10-18 14:38:58 +0800

    Edit Item/Add

--- app/(tabs)/EditItemScreen.tsx
+++ app/(tabs)/EditItemScreen.tsx
@@ -5,6 +5,7 @@ import { Image, StyleSheet, Pressable } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as ImagePicker from "expo-image-picker";
 
 
 export default function EditItemScreen(){
@@ -19,29 +20,31 @@ export default function EditItemScreen(){
       setQuantity(quantity - 1);
       }
     };
+
   return (
   <View className="flex-1 h-full relative z-0">
     <View 
       className="h-96 px-3"
       >
-      <Pressable className="absolute z-10 top-1 right-2.5 h-8">
+      <Pressable
+        className="absolute z-10 top-1 right-2.5 h-8">
         <FontAwesome name="edit" size={38} color="white" />
       </Pressable>
-      <Image
-        source={{
-          uri: 'https://bump2babynutrition.com/wp-content/uploads/2023/04/Sprite.jpg'
-        }}
-        className="w-full h-full rounded-3xl"
-      />
+        <Image
+          source={{
+            uri: 'https://bump2babynutrition.com/wp-content/uploads/2023/04/Sprite.jpg'
+            }}
+          className="w-full h-full rounded-3xl"
+            />
       <Text className="text-center text-3xl pt-3 text-green">$39</Text>
       <Text className="text-center text-4xl pt-3">Product Name</Text>
       <View className="h-36 justify-center items-center">
         <Text className="text-center text-base text-gray font-light px-3">Just a short product description or maybe nothing at all XD</Text>
       </View>
       </View>
-      <View className="flex flex-row h-16 absolute inset-x-0 bottom-0 w-full border-t-[0.5px] border-gray-300 items-left py-2 pl-3">
+      <View className="flex flex-row h-16 absolute inset-x-0 bottom-0 w-full border-t-[0.5px] border-gray-300 items-left py-2 pl-3 ">
         <View className="w-2/5">
-          <View className="w-32 h-full border-[0.5px] flex flex-row rounded-md items-center justify-center">
+          <View className="w-32 h-full border-[0.5px] flex flex-row rounded-md items-center justify-center shadow-md shadow-neutral-600">
             <Pressable className="border-r-[0.5px]" onPress={decrementQuantity}>
             <Text className="text-lg px-4">-</Text>
             </Pressable>
@@ -52,7 +55,7 @@ export default function EditItemScreen(){
             </View>
         </View>
         <View className="w-3/5 pr-3">
-          <Pressable className="w-full h-full rounded-md border items-center justify-center bg-green" onPress={decrementQuantity}>
+          <Pressable className="w-full h-full rounded-md items-center justify-center bg-green shadow-md shadow-neutral-600" onPress={decrementQuantity}>
             <Text className="text-center text-lg font-semibold text-white">Add to Cart</Text>
           </Pressable>
         </View>
