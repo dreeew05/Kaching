51b4392b3a10f674dd43103199099a5e2a8c3357
Glen Andrew Bulaong <gcbulaong@up.edu.ph>
2023-10-31 19:18:08 +0800

    Refactor Cart
    - Displays default image for empty cart
    
    Co-Authored-By: Palmsdale Kevin Dagohoy Cordero <99542164+Pawmsdayl@users.noreply.github.com>
    Co-Authored-By: Precious Kaira Saluria <91473395+kryshyr@users.noreply.github.com>

--- components/Cart/CartItemsGenerator.tsx
+++ components/Cart/CartItemsGenerator.tsx
@@ -1,15 +1,52 @@
 import { useSelector } from "react-redux";
 import { selectCart } from "../../redux/CartSelectors";
 import CartItemList from "./CartItemList";
+import { View, Image, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function CartItemsGenerator() {
 
     const cartState = useSelector(selectCart);
+    
+    const isCartEmpty = () => {
+        if(cartState.cart.length == 0) {
+            return true;
+        }
+    }
 
-    return(
-        <CartItemList
-            cart={cartState.cart}
-        />
-    )
+    if(isCartEmpty()) {
+        return(
+            <View className="flex-1">
+                <Text className="text-4xl ml-5 text-green"
+                    style={{fontFamily: 'Poppins-Bold'}}
+                >
+                    Cart
+                </Text>
+                <View className="flex-1 justify-center items-center">
+                    <FontAwesome name="cart-plus" size={300} color="black" />
+                    <Text className="text-2xl mt-5"
+                        style={{fontFamily: 'Poppins-Medium'}}
+                    >
+                        Your cart is empty
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+    else {
+        return(
+            <ScrollView>
+                <Text className="text-4xl ml-5 text-green"
+                    style={{fontFamily: 'Poppins-Bold'}}
+                >
+                    Cart
+                </Text>
+                <CartItemList
+                    cart={cartState.cart}
+                />
+            </ScrollView>
+        )
+    }
 
 }
