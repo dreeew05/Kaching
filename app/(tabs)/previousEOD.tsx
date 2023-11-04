112711aee6198b731140d53df7d12cdce1f3194f
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2023-11-04 21:51:57 +0800

    Implement Calendar picker
    Uses Modern Datepicker lib from:
    https://hosseinshabani.github.io/react-native-modern-datepicker/

--- app/(tabs)/previousEOD.tsx
+++ app/(tabs)/previousEOD.tsx
@@ -1,18 +1,50 @@
+import React, { useState } from 'react';
+import { Modal, Pressable } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import PreviousDatesScrollView from '../../components/PreviousDatesGenerator';
+import CalendarPicker from '../../components/CalendarPicker';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function TabTwoScreen() {
+  const [showCalendar, setShowCalendar] = useState(false);
+
   return (
-    <View className='flex-1 items-center justify-center'>
-      <View className='w-full flex-row justify-between px-6'>
-        <Text className='text-2xl'>
-          Recent EOD's
-        </Text>
-        <Ionicons name="calendar-sharp" size={30} color="#FFAD42" />
-      </View>
-        <PreviousDatesScrollView numDates={30} />
-    </View>
+    <>
+      {
+        showCalendar ? 
+        (  
+        <View className='flex-1'> 
+        <View className='flex items-end pr-4'>
+          <Pressable
+            onPress={() => setShowCalendar(false)}>
+            <Ionicons name="ios-close" size={30} color="black" />
+          </Pressable>
+        </View>
+        <View className='flex items-center py-6'>
+          <Text className='text-2xl font-bold pb-6 text-green'>
+            Choose a date
+          </Text>
+          <CalendarPicker />
+        </View>
+        </View>
+        ) 
+          : 
+        (
+        <View className='flex-1 items-center justify-center'>
+          <View className='w-full flex-row justify-between px-6'>
+            <Text className='text-2xl'>
+              Recent EOD's
+            </Text>
+            <Pressable
+              onPress={() => setShowCalendar(true)}>
+              <Ionicons name="calendar-sharp" size={30} color="#FFAD42" />
+            </Pressable>
+            </View>
+          <PreviousDatesScrollView numDates={31} />
+        </View>
+        )
+      }
+    </>
   );
 }
 ;
