dbffb0efe7b782b1623bbc549fa2054b2e4ff45a
Glen Andrew <gcbulaong@up.edu.ph>
2024-01-03 21:25:58 +0800

    Adding, Deleting, Updating Categories

--- components/DatabaseUtils/FetchInstructions/SelectAllCategories.ts
+++ components/DatabaseUtils/FetchInstructions/SelectAllCategories.ts
@@ -2,32 +2,29 @@ import { useEffect, useState } from "react";
 import { CategoryProps } from "../../__utils__/interfaces/CategoryProps";
 import { selectData } from "../CoreFunctions";
 import { useSelector } from "react-redux";
-import { RootState } from "../../../redux/Store";
 import { selectCategory } from "../../../redux/GlobalStateRedux/GlobalStateSelectors";
-// import { useCategoryContext } from "../../context/CategoryContext";
 
 export const selectAllCategories = () => {
-    
-    // const {actions, addAction} = useCategoryContext();
 
     const actionState = useSelector(selectCategory);
 
-    const tableName  = 'category',
-        column       = ['*'],
-        targetAttrib = null,
-        targetVal    = null;
+    const tableName    = 'category',
+          column       = ['*'],
+          targetAttrib = null,
+          targetValue  = null,
+          orderBy      = 'name';
 
     const [categoryData, setCategoryData] = useState<CategoryProps[]>([]);
 
     useEffect(() => {
-        selectData(tableName, column, targetAttrib, targetVal)
-        .then((result) => {
-            setCategoryData(result as CategoryProps[])
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+        selectData(tableName, column, targetAttrib, targetValue, orderBy)
+            .then((result) => {
+                setCategoryData(result as CategoryProps[])
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }, [actionState]); 
 
-    return categoryData
+    return categoryData;
 }
