60c4562ed7d534c624bb15b57eb7aab575a97a38
Glen Andrew <gcbulaong@up.edu.ph>
2024-01-04 15:58:12 +0800

    Updating Product [List, DetailedView]

--- app/(tabs)/ItemScreen.tsx
+++ app/(tabs)/ItemScreen.tsx
@@ -1,60 +1,24 @@
 import { useLocalSearchParams } from 'expo-router';
 import ParamsToInteger from '../../components/__utils__/helper/ParamsToInteger';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { Store } from '../../redux/Store';
 import { getDatabase } from '../../components/DatabaseUtils/OpenDatabase';
 import { useEffect, useState } from 'react';
 import { DetailedItemProps } from '../../components/__utils__/interfaces/DetailedItemProps';
 import DetailedItemScreen from '../../components/Product/DetailedItemScreen';
+import { selectProduct } from '../../redux/GlobalStateRedux/GlobalStateSelectors';
+import ItemScreenFetchData from '../../components/Product/ItemScreenFetchData';
 
 export default function ItemScreen() {
+
   const param = useLocalSearchParams();
   const id = ParamsToInteger(param.id);
 
-  const db = getDatabase();
-
-    const [product, setProduct] = useState<DetailedItemProps[]>([{
-        id: 0,
-        name: '',
-        image: '',
-        price: 0,
-        description: '',
-        category: ''
-    }]);
-
-    useEffect(() => {
-        db.transaction(tx => {
-            tx.executeSql(
-                `SELECT item.id, item.name, item.description, item.image, 
-                item.price,
-                category.name AS 'category'
-                FROM item
-                LEFT JOIN category ON item.category_id = category.id
-                WHERE item.id = ?`,
-                [id],
-                (_, result) => {
-                    setProduct(result.rows._array)
-                }
-            )
-        })
-    }, [id])
-
-    console.log(product)
-
-  return (
+  return(
     <Provider store={Store}>
-        <DetailedItemScreen
-            id={product[0].id}
-            name={product[0].name}
-            image={product[0].image}
-            price={product[0].price}
-            description={product[0].description}
-            category={product[0].category}
-        />
+      <ItemScreenFetchData
+        id={id}
+      />
     </Provider>
-  );
-  
-  // return(
-  //   <View></View>
-  // )
+  )
 }
