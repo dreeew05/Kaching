0718f7c02a3d0b047c927d11519f01506994b62d
kryshyr <pbsaluria@up.edu.ph>
2024-01-04 10:05:37 +0800

    Hide modifyItems tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -101,6 +101,14 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="modifyItems"
+        options={{
+          title: 'Modify Items',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          href: null, // Hide this tab from the tab bar
+        }}
+      />
+      <Tabs.Screen
         name="categoryView"
         options={{
           title: 'Back',
