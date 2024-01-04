f82b727d00b2c22ae4561b3bef5bafa320a06d94
Glen Andrew <gcbulaong@up.edu.ph>
2024-01-04 17:29:32 +0800

    Separate Components for Edit and Add Product

--- components/Product/ItemCard.tsx
+++ components/Product/ItemCard.tsx
@@ -11,11 +11,14 @@ import { addProductAction } from '../../redux/GlobalStateRedux/GlobalStateSlice'
 
 type itemCardProps = {
   item: BaseItemProps;
+  isEditComponent : boolean
 };
 
-export default function ItemCard({ item }: itemCardProps) {
+export default function ItemCard(item: itemCardProps) {
   const dispatch = useDispatch();
-  const itemState = useSelector((state: RootState) => selectCartItem(state, item.id));
+  const itemState = useSelector((state: RootState) => selectCartItem(
+    state, item.item.id
+  ));
 
   const [quantity, setQuantity] = useState(0);
 
@@ -26,12 +29,12 @@ export default function ItemCard({ item }: itemCardProps) {
   const addToCartEvent = () => {
     dispatch(
       addToCart({
-        id: item.id,
-        name: item.name,
-        price: item.price,
+        id: item.item.id,
+        name: item.item.name,
+        price: item.item.price,
         quantity: quantity,
-        image: item.image,
-        category: item.category,
+        image: item.item.image,
+        category: item.item.category,
       }),
     );
   };
@@ -54,41 +57,61 @@ export default function ItemCard({ item }: itemCardProps) {
         <Link
           href={{
             pathname: '/(tabs)/ItemScreen',
-            params: { id: item.id },
+            params: { id: item.item.id },
           }}
           asChild
         >
           <TouchableOpacity onPress={selectProduct}>
-            <Image className="w-40 h-40 mr-1 rounded-md" source={{uri:item.image}} />
+            <Image className="w-40 h-40 mr-1 rounded-md" 
+              source={{uri:item.item.image}} 
+            />
           </TouchableOpacity>
         </Link>
 
         <View className="flex flex-column ml-5">
           <Text className="text-lg font-semibold" style={{ fontFamily: 'Poppins-Medium' }}>
-            {item.name}
+            {item.item.name}
           </Text>
           <Text className="text-gray-500" style={{ fontFamily: 'Poppins-Regular' }}>
-            P{item.price}
+            P{item.item.price}
           </Text>
         </View>
       </View>
 
-      <View className="flex flex-row items-center">
-        <View className="flex items-center">
-          <Stepper id={item.id} quantity={quantity} updateQuantity={updateQuantity} />
+      {item.isEditComponent ? (
+        <View className="flex flex-row items-center">
+          <View className="flex items-center">
+            <Stepper id={item.item.id} quantity={quantity} updateQuantity={updateQuantity} />
+          </View>
+          <View className="flex-1 justify-center">
+            <Pressable
+              className="bg-green w-52 h-10 border-2 border-green 
+                          rounded-md self-center ml-6 flex-1 items-center justify-center"
+              onPress={addToCartEvent}
+            >
+              <Text className="text-white text-lg" style={{ fontFamily: 'Poppins-Bold' }}>
+                Add to Cart
+              </Text>
+            </Pressable>
+          </View>
         </View>
-        <View className="flex-1 justify-center">
-          <Pressable
-            className="bg-green w-52 h-10 border-2 border-green 
-                        rounded-md self-center ml-6 flex-1 items-center justify-center"
-            onPress={addToCartEvent}
-          >
-            <Text className="text-white text-lg" style={{ fontFamily: 'Poppins-Bold' }}>
-              Add to Cart
-            </Text>
-          </Pressable>
-        </View>
-      </View>
+        ) :
+        (
+          <View className="flex flex-row items-center">
+            <View className="flex-1 justify-center">
+              <Pressable
+                className="bg-green w-60 h-10 border-2 border-green 
+                            rounded-md self-center flex-1 items-center justify-center"
+                onPress={addToCartEvent}
+              >
+                <Text className="text-white text-lg" style={{ fontFamily: 'Poppins-Bold' }}>
+                  Edit Product
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        )
+      }
     </View>
   );
 }
