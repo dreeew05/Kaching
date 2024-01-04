6766acf3e0cdbd06396a1442042c00fa14f88bbb
kryshyr <pbsaluria@up.edu.ph>
2024-01-04 10:18:42 +0800

    Dynamically update category image

--- components/CustomPressable.tsx
+++ components/CustomPressable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, Text, View, TouchableHighlight } from 'react-native';
 
 type CustomPressableProps = {
   text: string;
@@ -9,7 +9,7 @@ type CustomPressableProps = {
 
 export default function CustomPressable({ text, onPress, disabled }: CustomPressableProps) {
   return (
-    <Pressable
+    <TouchableHighlight
       className={`w-3/5 self-center rounded-full p-3 mb-5 ${disabled ? 'bg-gray' : 'bg-green'}`} // Use the disabled prop to conditionally apply styles
       onPress={disabled ? undefined : onPress} // Prevent onPress if disabled
       disabled={disabled} // Set the disabled prop for accessibility
@@ -17,6 +17,6 @@ export default function CustomPressable({ text, onPress, disabled }: CustomPress
       <Text className={`text-white text-xl font-bold self-center ${disabled ? 'text-white' : ''}`}>
         {text}
       </Text>
-    </Pressable>
+    </TouchableHighlight>
   );
 }
