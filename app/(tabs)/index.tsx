637665124a6fa928c3c07602a11212904b9fa8c7
kryshyr <pbsaluria@up.edu.ph>
2024-01-09 07:35:16 +0800

    Start Day

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,49 +4,25 @@ import StoreInformationGenerator from '../../components/Home/StoreInformationGen
 import DayStarter from '../../components/Home/DayStarter';
 import SaleDashboard from '../../components/Home/SaleDashboard';
 import CategoryGenerator from '../../components/Home/CategoryGenerator';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { Store } from '../../redux/Store';
 import { getDatabase } from '../../components/DatabaseUtils/OpenDatabase';
 import { setIsEditComponent } from '../../redux/GlobalStateRedux/GlobalStateSlice';
+import { useEffect, useState } from 'react';
+
+
 
 export default function HomeScreen() {
   
   // Hide/Show start day pressable
-  let hasStartDayData = false;
-  let cashierName = 'Palmsdale Kevin'; // Replace with the actual cashier name
-
-  //check for start day data
-  const db = getDatabase();
-  // check if there iscurrent = 1 in eods table and if there is, set hasStartDayData to true
-  db.transaction((tx) => {
-    tx.executeSql(
-      'SELECT * FROM eods WHERE iscurrent = 1',
-      [],
-      (txObj, resultSet) => {
-        if (resultSet.rows.length > 0) {
-          hasStartDayData = true;
-          cashierName = resultSet.rows.item(0).cashiername;
-          console.log(cashierName);
-        }
-      },
-      // (txObj, error) => {
-      //   hasStartDayData = false;
-      // }
-    );
-  });
-  
-
   return (
     <Provider store={Store}>
       <View className="flex-1 self-stretch bg-white dark:bg-black">
         <ScrollView>
           <StoreInformationGenerator />
 
-          <DayStarter hasStartDayData={hasStartDayData} 
-            cashierName={cashierName} />
-          
-          <SaleDashboard />
-
+          <DayStarter />
+  
           <CategoryGenerator />
         </ScrollView>
       </View>
