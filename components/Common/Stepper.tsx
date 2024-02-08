d0999031abbfa621faa41435e08daadd477d553c
Glen Andrew <gcbulaong@up.edu.ph>
2024-02-08 23:29:06 +0800

    Add Modals
    Co-Authored-By: HelloCigar <100703625+HelloCigar@users.noreply.github.com>
    Co-Authored-By: Precious Kaira Saluria <91473395+kryshyr@users.noreply.github.com>

--- components/Common/Stepper.tsx
+++ components/Common/Stepper.tsx
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View, Pressable, Text } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-import { AntDesign } from '@expo/vector-icons';
 
 interface StepperProps {
   id: number;
   quantity: number;
   updateQuantity: (newQuantity: number) => void;
+  resetQuantity: () => void;
 }
 
 export default function Stepper(parentMixIn: StepperProps) {
   const [value, setValue] = useState(parentMixIn.quantity);
+
+  useEffect(() => {
+    if (value == 0) {
+      setValue(1);
+    }
+  }, [value]);
 
   const incrementQuantity = () => {
     setValue(value + 1);
@@ -24,49 +30,54 @@ export default function Stepper(parentMixIn: StepperProps) {
     }
   };
 
-  const handleTextChange = (val : string) => {
-    if(!Number.isNaN(parseInt(val))) {
+  const handleTextChange = (val: string) => {
+    if (!Number.isNaN(parseInt(val))) {
       setValue(parseInt(val));
       parentMixIn.updateQuantity(parseInt(val));
-    }
-    else {
+    } else {
       setValue(1);
       parentMixIn.updateQuantity(1);
     }
-  }
+  };
 
   return (
     <View
       className="flex flex-row items-left w-40 h-10 rounded-md items-center
         justify-center border-[1px]"
     >
-      <View className='items-center justify-center'>
+      <View className="items-center justify-center">
         <Pressable onPress={decrementQuantity}>
-        <Text className="text-lg px-5" style={{ fontFamily: 'Poppins-Medium' }}>
+          <Text
+            className="text-lg px-5"
+            style={{ fontFamily: 'Poppins-Medium' }}
+          >
             -
           </Text>
         </Pressable>
       </View>
 
-      <TextInput className="text-lg px-5 border-[1px] h-10 items-center
-        justify-center" 
-        style={{ 
-          fontFamily: 'Poppins-Medium', 
+      <TextInput
+        className="text-lg px-5 border-[1px] h-10 items-center
+        justify-center"
+        style={{
+          fontFamily: 'Poppins-Medium',
           textAlign: 'center',
         }}
         maxLength={3}
-        keyboardType='numeric'
+        keyboardType="numeric"
         onChangeText={(val) => handleTextChange(val)}
       >
         {value}
       </TextInput>
 
       <Pressable onPress={incrementQuantity}>
-        <Text className="text-lg px-5" style={{ fontFamily: 'Poppins-Medium' }}>
+        <Text
+          className="text-lg px-5"
+          style={{ fontFamily: 'Poppins-Medium' }}
+        >
           +
         </Text>
       </Pressable>
-
     </View>
   );
 }
