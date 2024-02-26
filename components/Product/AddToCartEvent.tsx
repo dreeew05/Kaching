1ebf3b162c0c6b492ee3b9c2e3b0b5ba3bd33bdb
Glen Andrew <gcbulaong@up.edu.ph>
2024-02-26 18:49:40 +0800

    Remove "Item In Cart" PopUp

--- components/Product/AddToCartEvent.tsx
+++ components/Product/AddToCartEvent.tsx
@@ -2,23 +2,19 @@ import { addToCart } from '../../redux/CartRedux/CartSlice';
 import { AddToCartInterface } from '../__utils__/interfaces/AddToCartInterface';
 
 export const addToCartEvent = (item: AddToCartInterface) => {
-  if (item.itemState == undefined) {
-    if (item.quantity > 0) {
-      item.dispatch(
-        addToCart({
-          id: item.product.id,
-          name: item.product.name,
-          price: item.product.price,
-          quantity: item.quantity,
-          image: item.product.image,
-          category: item.product.category,
-        }),
-      );
-      item.showAddModal(true);
-    } else {
-      item.showAddQuantityModal(true);
-    }
+  if (item.quantity > 0) {
+    item.dispatch(
+      addToCart({
+        id: item.product.id,
+        name: item.product.name,
+        price: item.product.price,
+        quantity: item.quantity,
+        image: item.product.image,
+        category: item.product.category,
+      }),
+    );
+    item.showAddModal(true);
   } else {
-    item.showItemInCartModal(true);
+    item.showAddQuantityModal(true);
   }
 };
