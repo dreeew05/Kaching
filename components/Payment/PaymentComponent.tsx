044a61f56d47c4caaf258ea8a8e3c212f406ced3
kryshyr <pbsaluria@up.edu.ph>
2024-04-02 02:39:34 +0800

    UI Fix: Payment

--- components/Payment/PaymentComponent.tsx
+++ components/Payment/PaymentComponent.tsx
@@ -1,18 +1,19 @@
 import { useRouter } from 'expo-router';
-import { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
-import CustomPressable from '../Common/CustomPressable';
+import { RFPercentage } from 'react-native-responsive-fontsize';
 import { useSelector } from 'react-redux';
 import {
   selectCartItems,
   selectCartTotalPrice,
 } from '../../redux/CartRedux/CartSelectors';
+import CustomPressable from '../Common/CustomPressable';
 import { insertData } from '../DatabaseUtils/CoreFunctions';
-import { generateUniqueId } from '../__utils__/helper/GenerateUniqueId';
 import { getDatabase } from '../DatabaseUtils/OpenDatabase';
 import CustomModal from '../Modals/CustomModal';
 import { PopUpModal } from '../Modals/PopUpModal';
+import { generateUniqueId } from '../__utils__/helper/GenerateUniqueId';
 
 export default function PaymentComponent() {
   const [open, setOpen] = useState(false);
@@ -109,7 +110,7 @@ export default function PaymentComponent() {
       tx.executeSql(
         `INSERT INTO eod_receipts (eod_id, receipt_id)
           VALUES ((SELECT eod_id FROM eods WHERE iscurrent = 1),
-            (SELECT receipt_id FROM receipts ORDER BY receipt_id 
+            (SELECT receipt_id FROM receipts ORDER BY receipt_id
               DESC LIMIT 1
             )
           )`,
@@ -129,82 +130,122 @@ export default function PaymentComponent() {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'space-between',
-      }}
-      className="flex-1 self-stretch bg-white dark-bg-black"
-    >
-      <View>
+    <View className="flex-1 bg-white dark-bg-black">
+      <View className="flex flex-col flex-1">
         <Text
-          className="text-3xl self-center mt-10 text-green"
+          numberOfLines={1}
+          className=" text-2xl sm:text-2xl md:text-4xl lg:text-5xl xl:text-3xl font-semibold self-center mt-10  text-green"
           style={{ fontFamily: 'Poppins-Medium' }}
         >
-          Payment
+          Amount to pay
         </Text>
         <Text
-          className="text-4xl mt-5 font-medium 
+          className="text-5xl sm:text-5xl md:text-7xl lg:text-9xl xl:text-3xl mt-5 font-medium
                 p-5 text-yellow self-center"
           style={{ fontFamily: 'Poppins-Medium' }}
         >
-          PHP {totalCartPrice.toFixed(2)}
-        </Text>
-        <Text
-          className="text-base mb-2 font-base text-gray self-center"
-          style={{ fontFamily: 'Poppins-Medium' }}
-        >
-          Please select a mode of payment
+          ₱ {totalCartPrice.toFixed(2)}
         </Text>
 
-        <DropDownPicker
-          style={{
-            backgroundColor: 'lightgray',
-            borderColor: 'lightgray',
-            borderWidth: 2,
-          }}
-          textStyle={{
-            fontSize: 14,
-            fontWeight: 'bold',
-            color: 'gray',
-          }}
-          containerStyle={{
-            height: 40,
-            width: 275,
-            alignSelf: 'center',
-          }}
-          dropDownContainerStyle={{
-            backgroundColor: 'white',
-            borderColor: 'lightgray',
-          }}
-          itemSeparator={true}
-          itemSeparatorStyle={{
-            backgroundColor: 'lightgray',
-            height: 1,
-          }}
-          placeholder="Mode of Payment"
-          open={open}
-          value={modeOfPayment}
-          items={items}
-          setOpen={setOpen}
-          setValue={setModeOfPayment}
-          setItems={setItems}
-          onOpen={() => handleDropdownToggle(true)}
-          onClose={() => handleDropdownToggle(false)}
-        />
+        <View className=" flex-1 justify-center">
+          <Text
+            className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-2xl mb-2 font-base text-gray self-center"
+            style={{ fontFamily: 'Poppins-Medium' }}
+          >
+            Please select a mode of payment
+          </Text>
+          <View className="self-center mt-2 w-2/3 sm:w-2/3 md:w-1/2 lg:w-3/5 xl:w-3/5">
+            <DropDownPicker
+              className=" border-0 rounded-xl  h-12 sm:h-20 md:h-20 lg:h-24 xl:h-24"
+              style={{
+                backgroundColor: '#FFAD42',
+                borderColor: '#FFAD42',
+                borderWidth: 2,
+                borderRadius: 10,
+              }}
+              arrowIconStyle={{
+                // change arrow color
+                tintColor: 'white',
+              }}
+              containerStyle={{
+                backgroundColor: 'white',
+                alignSelf: 'center',
+                zIndex: 1,
+              }}
+              dropDownContainerStyle={{
+                backgroundColor: 'white',
+                borderColor: '#FFAD42',
+                borderWidth: 3,
+                maxHeight: 150,
+              }}
+              itemSeparator={true}
+              itemSeparatorStyle={{
+                backgroundColor: '#FFAD42',
+                height: 3,
+              }}
+              labelStyle={{
+                fontSize: RFPercentage(2.5),
+                color: 'white',
+                fontWeight: 'bold',
+                marginLeft: 10,
+              }}
+              listItemLabelStyle={{
+                fontSize: RFPercentage(2),
+                color: '#636363',
+                fontWeight: 'bold',
+                marginLeft: 10,
+                height: 50,
+                lineHeight: 50,
+              }}
+              listItemContainerStyle={{
+                height: 50, // Adjust the height according to your preference
+              }}
+              selectedItemContainerStyle={{
+                backgroundColor: '#ffdfb5',
+              }}
+              placeholder="Mode of Payment"
+              // change placeholder text color
+              placeholderStyle={{
+                color: 'white',
+                fontWeight: 'bold',
+                marginLeft: 10,
+                fontSize: RFPercentage(2),
+              }}
+              open={open}
+              value={modeOfPayment}
+              items={items}
+              setOpen={setOpen}
+              setValue={setModeOfPayment}
+              setItems={setItems}
+              onOpen={() => handleDropdownToggle(true)}
+              onClose={() => handleDropdownToggle(false)}
+            />
+          </View>
 
-        <TextInput
-          className="border-2 border-gray rounded-xl p-2 m-5 mt-40 self-center w-2/3 text-gray text-base font-semibold"
-          onChangeText={onChangeUserPayment}
-          value={userPayment}
-          placeholder="Enter Payment Amount"
-          keyboardType="decimal-pad"
-          style={{ marginTop: inputMargin }}
-        />
-      </View>
-
-      <View>
-        <CustomPressable text="Confirm Payment" onPress={showModal} />
+          <View className="justify-center mt-8">
+            <TextInput
+              className=" self-center border-2 border-green rounded-xl p-2 w-2/3 sm:w-2/3 md:w-1/2 lg:w-3/5 xl:w-3/5 h-12 sm:h-20 md:h-20 lg:h-24 xl:h-24  text-gray text-base font-semibold"
+              onChangeText={onChangeUserPayment}
+              value={userPayment}
+              placeholder="Enter Payment Amount"
+              placeholderTextColor={'green'}
+              // style the placeholder
+              style={{
+                fontWeight: 'bold',
+                color: 'green',
+                fontSize: RFPercentage(2),
+                paddingLeft: 20,
+              }}
+              keyboardType="decimal-pad"
+            />
+          </View>
+        </View>
+        <View className="flex-1 justify-end">
+          <CustomPressable
+            text="Confirm Payment"
+            onPress={showModal}
+          />
+        </View>
       </View>
 
       <CustomModal
